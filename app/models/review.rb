73b3a662b1a89b5488a1cbf1b578b9995957a9a6
Frederique Petris <frederique.petris@gmail.com>
2015-11-11 14:18:05 +0100

    seed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :restaurant_id, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Restaurant.destroy_all
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+  }
+]
+restaurants_attributes.each { |params| Restaurant.create!(params) }
